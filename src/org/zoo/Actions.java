af0a7fe8c2f374a5e41c56fade8987680370a7b3
MarcoPercolla <percollamarco@virgilio.com>
2024-05-22 15:38:36 +0200

    add step 2 done

---
+++ src/org/zoo/Actions.java
@@ -0,0 +1,11 @@
+package org.zoo;
+
+public class Actions {
+    public void doFly(CanFly animale) {
+        animale.fly();
+    }
+
+    public void doSwim(CanSwim animale) {
+        animale.swim();
+    }
+}
--- src/org/zoo/Zoo.java
+++ src/org/zoo/Zoo.java
@@ -2,21 +2,30 @@ package org.zoo;
 
 public class Zoo {
     public static void main(String[] args) {
-        Animal[] zoo = {new Dog(), new Bird(), new Eagle(), new Dolphin()};
+//        Animal[] zoo = {new Dog(), new Bird(), new Eagle(), new Dolphin()};
+//
+//        for (Animal animale : zoo) {
+//            animale.sleep();
+//            animale.makeASound();
+//            animale.eat();
+//
+//            if (animale instanceof CanFly) {
+//                ((CanFly) animale).fly();
+//            }
+//
+//            if (animale instanceof CanSwim) {
+//                ((CanSwim) animale).swim();
+//            }
+//        }
 
-        for (Animal animale : zoo) {
-            animale.sleep();
-            animale.makeASound();
-            animale.eat();
+        CanSwim delfino = new Dolphin();
+        CanFly aquila = new Eagle();
+        CanFly uccello = new Bird();
+        Actions actions = new Actions();
 
-            if (animale instanceof CanFly) {
-                ((CanFly) animale).fly();
-            }
-
-            if (animale instanceof CanSwim) {
-                ((CanSwim) animale).swim();
-            }
-        }
+        actions.doFly(aquila);
+        actions.doFly(uccello);
+        actions.doSwim(delfino);
 
 
 
